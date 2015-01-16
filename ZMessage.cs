8fb4107d664ea5c64fb2396677e02bbbbfa945d8
metadings <metadings@github.com>
2015-01-16 03:36:30 +0100

    Improving ZMessage.Read and .Write Methods

--- ZMessage.cs
+++ ZMessage.cs
@@ -1,4 +1,6 @@
-﻿namespace ZeroMQ
+﻿using System.Text;
+
+namespace ZeroMQ
 {
 	using System;
 	using System.Collections;
@@ -79,37 +81,13 @@
 			}
 		}
 
-		public override bool CanRead
-		{
-			get
-			{
-				return true;
-			}
-		}
+		public override bool CanRead { get { return true; } }
 
-		public override bool CanSeek
-		{
-			get
-			{
-				return false;
-			}
-		}
+		public override bool CanSeek { get { return false; } }
 
-		public override bool CanTimeout
-		{
-			get
-			{
-				return false;
-			}
-		}
+		public override bool CanTimeout { get { return false; } }
 
-		public override bool CanWrite
-		{
-			get
-			{
-				return true;
-			}
-		}
+		public override bool CanWrite { get { return true; } }
 
 		public override void SetLength(long value)
 		{
@@ -180,6 +158,11 @@
 			return frame; // Math.Min(requiredBytes, size); // remaining bytes or how much are required
 		}
 
+		private ZFrame MoveNextFrame() 
+		{
+			return MoveNextFrame(ZFrame.DefaultFrameSize);
+		}
+
 		private ZFrame MoveNextFrame(int size)
 		{
 			if (_current + 1 >= _frames.Count)
@@ -236,6 +219,138 @@
 			return frame.ReadByte();
 		}
 
+
+		public virtual Int16 ReadInt16()
+		{
+			var bytes = new byte[2];
+			int len = Read(bytes, 0, 2);
+			if (len < 2)
+			{
+				return default(Int16);
+			}
+
+			return BitConverter.ToInt16(bytes, 0);
+		}
+
+		public virtual UInt16 ReadUInt16()
+		{
+			var bytes = new byte[2];
+			int len = Read(bytes, 0, 2);
+			if (len < 2)
+			{
+				return default(UInt16);
+			}
+
+			return BitConverter.ToUInt16(bytes, 0);
+		}
+
+		public virtual Char ReadChar()
+		{
+			var bytes = new byte[2];
+			int len = Read(bytes, 0, 2);
+			if (len < 2)
+			{
+				return default(Char);
+			}
+
+			return BitConverter.ToChar(bytes, 0);
+		}
+
+		public virtual Int32 ReadInt32()
+		{
+			var bytes = new byte[4];
+			int len = Read(bytes, 0, 4);
+			if (len < 4)
+			{
+				return default(Int32);
+			}
+
+			return BitConverter.ToInt32(bytes, 0);
+		}
+
+		public virtual UInt32 ReadUInt32()
+		{
+			var bytes = new byte[4];
+			int len = Read(bytes, 0, 4);
+			if (len < 4)
+			{
+				return default(UInt32);
+			}
+
+			return BitConverter.ToUInt32(bytes, 0);
+		}
+
+		public virtual Single ReadSingle()
+		{
+			var bytes = new byte[4];
+			int len = Read(bytes, 0, 4);
+			if (len < 4)
+			{
+				return default(Single);
+			}
+
+			return BitConverter.ToSingle(bytes, 0);
+		}
+
+		public virtual Int64 ReadInt64()
+		{
+			var bytes = new byte[8];
+			int len = Read(bytes, 0, 8);
+			if (len < 8)
+			{
+				return default(Int64);
+			}
+
+			return BitConverter.ToInt64(bytes, 0);
+		}
+
+		public virtual UInt64 ReadUInt64()
+		{
+			var bytes = new byte[8];
+			int len = Read(bytes, 0, 8);
+			if (len < 8)
+			{
+				return default(UInt64);
+			}
+
+			return BitConverter.ToUInt64(bytes, 0);
+		}
+
+		public virtual Double ReadDouble()
+		{
+			var bytes = new byte[8];
+			int len = Read(bytes, 0, 8);
+			if (len < 8)
+			{
+				return default(Double);
+			}
+
+			return BitConverter.ToDouble(bytes, 0);
+		}
+
+		public string ReadString()
+		{
+			return ReadString(ZContext.Encoding);
+		}
+
+		public string ReadString(Encoding encoding)
+		{
+			return ReadString( /* byteCount */ (int)Length, encoding);
+		}
+
+		public string ReadString(int length)
+		{
+			return ReadString( /* byteCount */ length, ZContext.Encoding);
+		}
+
+		public string ReadString(int length, Encoding encoding)
+		{
+			int byteCount = encoding.GetMaxByteCount(length);
+			var bytes = new byte[byteCount];
+			Read(bytes, 0, byteCount);
+			return encoding.GetString(bytes);
+		}
+
 		public override void Write(byte[] buffer, int offset, int count)
 		{
 			ZFrame current = CurrentFrame;
@@ -258,7 +373,7 @@
 				// length += toWrite;
 				count -= toWrite;
 
-			} while (count > 0 && null != (current = MoveNextFrame(ZFrame.DefaultFrameSize)));
+			} while (count > 0 && null != (current = MoveNextFrame()));
 			// return length;
 		}
 
@@ -275,6 +390,77 @@
 				throw new InvalidOperationException();
 			}
 			frame.WriteByte(value);
+		}
+
+		public void WriteInt16(Int16 value)
+		{
+			if (Position + 2 > Length)
+			{
+				throw new InvalidOperationException();
+			}
+			byte[] bytes = BitConverter.GetBytes(value);
+			Write(bytes, 0, bytes.Length);
+		}
+
+		public void WriteUInt16(UInt16 value)
+		{
+			WriteInt16((Int16)value);
+		}
+
+		public void WriteChar(Char value)
+		{
+			WriteInt16((Int16)value);
+		}
+
+		public void WriteInt32(Int32 value)
+		{
+			if (Position + 4 > Length)
+			{
+				throw new InvalidOperationException();
+			}
+			byte[] bytes = BitConverter.GetBytes(value);
+			Write(bytes, 0, bytes.Length);
+		}
+
+		public void WriteUInt32(UInt32 value)
+		{
+			WriteInt32((Int32)value);
+		}
+
+		public void WriteSingle(Single value)
+		{
+			WriteInt32((Int32)value);
+		}
+
+		public void WriteInt64(Int64 value)
+		{
+			if (Position + 8 > Length)
+			{
+				throw new InvalidOperationException();
+			}
+			byte[] bytes = BitConverter.GetBytes(value);
+			Write(bytes, 0, bytes.Length);
+		}
+
+		public void WriteUInt64(UInt64 value)
+		{
+			WriteInt64((Int64)value);
+		}
+
+		public void WriteDouble(Double value)
+		{
+			WriteInt64((Int64)value);
+		}
+
+		public void WriteString(string str)
+		{
+			WriteString(str, ZContext.Encoding);
+		}
+
+		public void WriteString(string str, Encoding encoding)
+		{
+			var bytes = encoding.GetBytes(str);
+			Write(bytes, 0, bytes.Length);
 		}
 
 		public override void Flush()
