83c423ba8168e9fdd90cd9c82d3f200078d3d496
metadings <metadings@github.com>
2015-01-22 16:07:41 +0100

    Improving ZPollItem

--- ZPollItem.cs
+++ ZPollItem.cs
@@ -5,8 +5,6 @@ namespace ZeroMQ
 
 	public class ZPollItem
 	{
-		// public ZSocket Socket { get; protected set; }
-
 		public ZPoll Events;
 
 		public ZPoll ReadyEvents;
@@ -30,48 +28,42 @@ namespace ZeroMQ
 			return socket.Send(message, out error);
 		}
 
-		protected ZPollItem(/* ZSocket socket, */ ZPoll events)
+		protected ZPollItem(ZPoll events)
 		{
-			/* if (socket == null)
-			{
-				throw new ArgumentNullException("socket");
-			}
-
-			Socket = socket; */
 			Events = events;
 		}
 
-		public static ZPollItem Create(/* ZSocket socket, */ ReceiveDelegate receiveMessage)
+		public static ZPollItem Create(ReceiveDelegate receiveMessage)
 		{
-			return Create(/* socket, */ receiveMessage, null);
+			return Create(receiveMessage, null);
 		}
 
-		public static ZPollItem CreateSender(/* ZSocket socket, */ SendDelegate sendMessage)
+		public static ZPollItem CreateSender(SendDelegate sendMessage)
 		{
-			return Create(/* socket, */ null, sendMessage);
+			return Create(null, sendMessage);
 		}
 
-		public static ZPollItem Create(/* ZSocket socket, */ ReceiveDelegate receiveMessage, SendDelegate sendMessage)
+		public static ZPollItem Create(ReceiveDelegate receiveMessage, SendDelegate sendMessage)
 		{
-			var pollItem = new ZPollItem(/* socket, */ (receiveMessage != null ? ZPoll.In : ZPoll.None) | (sendMessage != null ? ZPoll.Out : ZPoll.None));
+			var pollItem = new ZPollItem((receiveMessage != null ? ZPoll.In : ZPoll.None) | (sendMessage != null ? ZPoll.Out : ZPoll.None));
 			pollItem.ReceiveMessage = receiveMessage;
 			pollItem.SendMessage = sendMessage;
 			return pollItem;
 		}
 
-		public static ZPollItem CreateReceiver(/* ZSocket socket, */ )
+		public static ZPollItem CreateReceiver()
 		{
-			return Create(/* socket, */ DefaultReceiveMessage, null);
+			return Create(DefaultReceiveMessage, null);
 		}
 
-		public static ZPollItem CreateSender(/* ZSocket socket, */ )
+		public static ZPollItem CreateSender()
 		{
-			return Create(/* socket, */ null, DefaultSendMessage);
+			return Create(null, DefaultSendMessage);
 		}
 
-		public static ZPollItem CreateReceiverSender(/* ZSocket socket, */ )
+		public static ZPollItem CreateReceiverSender()
 		{
-			return Create(/* socket, */ DefaultReceiveMessage, DefaultSendMessage);
+			return Create(DefaultReceiveMessage, DefaultSendMessage);
 		}
 	}
 }
