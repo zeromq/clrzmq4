5a4f75aa0e2cc4fcf5da462db4ea00003a9a3fa3
metadings <metadings@live.de>
2015-01-30 14:56:14 +0100

    Updating AssemblyInfo

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -3,5 +3,5 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("clrzmq")]
+[assembly: AssemblyTitle("ZeroMQ")]
 [assembly: AssemblyDescription("ZeroMQ .NET bindings")]
