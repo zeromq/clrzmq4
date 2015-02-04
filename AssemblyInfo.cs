8cf3b3747344e99d1c004095638e181217e43e87
metadings <metadings@live.de>
2015-02-04 16:25:43 +0100

    Updating nuget Package

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("ZeroMQ CLR namespace")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ØMQ")]
-[assembly: AssemblyProduct("ZeroMQ")]
-[assembly: AssemblyCopyright("metadings")]
+[assembly: AssemblyProduct("ØMQ")]
+[assembly: AssemblyCopyright("ØMQ")]
 [assembly: AssemblyTrademark("")]
 // [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.4.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
