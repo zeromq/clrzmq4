839e2c2c0956ef935172e4c02a79d60c417907b6
metadings <metadings@live.de>
2015-02-20 00:53:44 +0100

    Updating ZeroMQ

--- packages/Baseclass.Contrib.Nuget.GitIgnoreContent.0.0.2/tools/RestoreNugetContent.ps1
+++
@@ -1,7 +0,0 @@
-﻿$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
-
-$modulePath = Join-Path $PSScriptRoot Baseclass.Contrib.Nuget.GitIgnoreContent.psm1
-
-Import-Module $modulePath
-
-Restore-NugetContentFiles $PSScriptRoot
