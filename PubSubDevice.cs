38b2b941b91869db808f1b2553cb25af1f0120cb
metadings <metadings@github.com>
2015-02-20 02:51:00 +0100

    Updating Devices

--- PubSubDevice.cs
+++ PubSubDevice.cs
@@ -3,13 +3,8 @@
 	using System;
 
 	/// <summary>
-	/// Collects messages from a set of publishers and forwards these to a set of subscribers.
+	/// Device for a Publisher and Subscribers
 	/// </summary>
-	/// <remarks>
-	/// Generally used to bridge networks. E.g. read on TCP unicast and forward on multicast.
-	/// This device is part of the publish-subscribe pattern. The frontend speaks to publishers
-	/// and the backend speaks to subscribers.
-	/// </remarks>
 	public class PubSubDevice : ZDevice
 	{
 		/// <summary>
@@ -21,13 +16,10 @@
 		/// The backend <see cref="ZSocketType"/> for a forwarder device.
 		/// </summary>
 		public static readonly ZSocketType BackendType = ZSocketType.XPUB;
+
 		/// <summary>
 		/// Initializes a new instance of the <see cref="ForwarderDevice"/> class.
 		/// </summary>
-		/// <param name="context">The <see cref="ZContext"/> to use when creating the sockets.</param>
-		/// <param name="frontendBindAddr">The endpoint used to bind the frontend socket.</param>
-		/// <param name="backendBindAddr">The endpoint used to bind the backend socket.</param>
-		/// <param name="mode">The <see cref="DeviceMode"/> for the current device.</param>
 		public PubSubDevice(ZContext context, string frontendBindAddr, string backendBindAddr)
 			: base(context, FrontendType, BackendType)
 		{
@@ -39,16 +31,14 @@
 		/// <summary>
 		/// Forwards requests from the frontend socket to the backend socket.
 		/// </summary>
-		/// <param name="args">A <see cref="SocketEventArgs"/> object containing the poll event args.</param>
 		protected override bool FrontendHandler(ZSocket socket, out ZMessage message, out ZError error)
 		{
 			return FrontendSocket.Forward(BackendSocket, out message, out error);
 		}
 
 		/// <summary>
-		/// Not implemented for the <see cref="ForwarderDevice"/>.
+		/// PubSub Forwards the Subscription messages
 		/// </summary>
-		/// <param name="args">A <see cref="SocketEventArgs"/> object containing the poll event args.</param>
 		protected override bool BackendHandler(ZSocket args, out ZMessage message, out ZError error)
 		{
 			return BackendSocket.Forward(FrontendSocket, out message, out error);
