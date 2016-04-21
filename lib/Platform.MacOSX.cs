fa0d7c9067f25b842168eae4fc72c2b97fb4cf99
metadings <metadings@live.de>
2016-04-21 12:54:37 +0200

    Removing lib.Platform.LibraryFileExtension, in favor of lib.Platform.LibraryPaths, on MacOSX

--- lib/Platform.MacOSX.cs
+++ lib/Platform.MacOSX.cs
@@ -6,7 +6,28 @@
 		public static class MacOSX
 		{
 
-			public const string LibraryFileExtension = ".dylib";
+			// public const string LibraryFileExtension = ".dylib";
+
+			public static readonly string[] LibraryPaths = new string[] {
+				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.a",
+				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.a.*",
+				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.so",
+				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.so.*",
+				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.dylib",
+				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.dylib.*",
+				"{AppBase}/{Arch}/{LibraryName}.a",
+				"{AppBase}/{Arch}/{LibraryName}.a.*",
+				"{AppBase}/{Arch}/{LibraryName}.so",
+				"{AppBase}/{Arch}/{LibraryName}.so.*",
+				"{AppBase}/{Arch}/{LibraryName}.dylib",
+				"{AppBase}/{Arch}/{LibraryName}.dylib.*",
+				"{Path}/{LibraryName}.a",
+				"{Path}/{LibraryName}.a.*",
+				"{Path}/{LibraryName}.so",
+				"{Path}/{LibraryName}.so.*",
+				"{Path}/{LibraryName}.dylib",
+				"{Path}/{LibraryName}.dylib.*",
+			};
 
 		}
 	}
