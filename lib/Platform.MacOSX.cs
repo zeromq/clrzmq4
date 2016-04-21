5637e9f06b7a0d4232da43b6f75107ec887595dc
metadings <metadings@live.de>
2016-04-21 20:59:08 +0200

    Updating lib.Platform.MacOSX: Preferring .dylib

--- lib/Platform.MacOSX.cs
+++ lib/Platform.MacOSX.cs
@@ -9,24 +9,24 @@
 			// public const string LibraryFileExtension = ".dylib";
 
 			public static readonly string[] LibraryPaths = new string[] {
+				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.dylib",
+				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.dylib.*",
 				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.a",
 				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.a.*",
 				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.so",
 				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.so.*",
-				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.dylib",
-				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.dylib.*",
+				"{AppBase}/{Arch}/{LibraryName}.dylib",
+				"{AppBase}/{Arch}/{LibraryName}.dylib.*",
 				"{AppBase}/{Arch}/{LibraryName}.a",
 				"{AppBase}/{Arch}/{LibraryName}.a.*",
 				"{AppBase}/{Arch}/{LibraryName}.so",
 				"{AppBase}/{Arch}/{LibraryName}.so.*",
-				"{AppBase}/{Arch}/{LibraryName}.dylib",
-				"{AppBase}/{Arch}/{LibraryName}.dylib.*",
+				"{Path}/{LibraryName}.dylib",
+				"{Path}/{LibraryName}.dylib.*",
 				"{Path}/{LibraryName}.a",
 				"{Path}/{LibraryName}.a.*",
 				"{Path}/{LibraryName}.so",
 				"{Path}/{LibraryName}.so.*",
-				"{Path}/{LibraryName}.dylib",
-				"{Path}/{LibraryName}.dylib.*",
 			};
 
 		}
