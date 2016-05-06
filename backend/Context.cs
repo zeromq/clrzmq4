f5640cb6feb94a40fbc706e92d02928ae5e6b739
Stephen Nuchia <snuchia@gmail.com>
2016-05-06 04:30:41 +0000

    moving pyzmq backend glue logic for cli to c#, in progress.

---
+++ backend/Context.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroMQ
+{
+    public static partial class backend
+    {
+        public class Context
+        {
+            internal readonly ZContext _ZContext;
+            internal Context(ZContext zc)
+            {
+                if (zc == null)
+                    zc = ZContext.Create();
+                _ZContext = zc;
+            }
+            public Context(int io_threads = 1)
+            {
+                var zc = ZContext.Create();
+                zc.ThreadPoolSize = io_threads;
+                _ZContext = zc;
+            }
+            public bool closed { get { return _ZContext == null || _ZContext.ContextPtr == IntPtr.Zero; } }
+            public Socket socket(ZSocketType st)
+            {
+                return new Socket(new ZSocket(_ZContext, st));
+            }
+        }
+    }
+}
---
+++ backend/Socket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroMQ
+{
+    public static partial class backend
+    {
+        public class Socket
+        {
+            internal readonly ZSocket _ZSocket;
+            internal Socket(ZSocket s)
+            {
+                _ZSocket = s;
+            }
+            //public Socket(Context ctx, ZSocketType st)
+            //{
+            //    _ZSocket = ctx.socket(st);
+            //}
+            public Socket()
+            {
+                throw new NotImplementedException("ZeroMQ.backend Socket default ctor should not be used");
+                // required to exist for python to subclass it.
+            }
+            public void set(ZSocketOption so, IEnumerable<Byte> buf)
+            {
+                _ZSocket.SetOption(so, buf.ToArray());
+            }
+            public IEnumerable<Byte> get(ZSocketOption so)
+            {
+                return _ZSocket.GetOptionBytes(so);
+            }
+
+            public bool closed { get { return _ZSocket.SocketPtr == null; } }
+        }
+    }
+}
