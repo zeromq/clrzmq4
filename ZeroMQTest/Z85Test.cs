f77e4dd5e130c610d13f97d932f2d1ceea7a32b9
Simon Giesecke <simon.giesecke@gmail.com>
2017-05-20 13:43:10 +0200

    Fixed test case

--- ZeroMQTest/Z85Test.cs
+++ ZeroMQTest/Z85Test.cs
@@ -11,8 +11,10 @@ namespace ZeroMQTest
 			byte[] secretKey;
 			ZeroMQ.Z85.CurveKeypair(out publicKey, out secretKey);
 
-			Assert.AreEqual(32, publicKey.Length);
-			Assert.AreEqual(32, secretKey.Length);
+			// TODO: apparently, CurveKeyPair generates Z85 keys... which is confusing as it has byte[] rather than 
+			// string output parameters
+			Assert.AreEqual(40, publicKey.Length);
+			Assert.AreEqual(40, secretKey.Length);
 			CollectionAssert.AreNotEqual(publicKey, secretKey);
 		}
 	}
