c65ffda9d7627e48345452d14eba61a3b2fc7ac2
Simon Giesecke <simon.giesecke@gmail.com>
2017-05-20 13:17:55 +0200

    Added solution file and initial test with nunit

---
+++ ZeroMQTest/Z85Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+namespace ZeroMQTest
+{
+	[TestFixture]
+	public class Z85Test
+	{
+		[Test]
+		public void CurveKeypair()
+		{
+			byte[] publicKey;
+			byte[] secretKey;
+			ZeroMQ.Z85.CurveKeypair(out publicKey, out secretKey);
+
+			Assert.AreEqual(32, publicKey.Length);
+			Assert.AreEqual(32, secretKey.Length);
+			CollectionAssert.AreNotEqual(publicKey, secretKey);
+		}
+	}
+}
