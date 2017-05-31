574b3fae4fb90d9c0463300d1c65a8fa583dfd07
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-05-31 17:50:53 +0200

    Added some tests for ZMessage

---
+++ ZeroMQTest/ZMessageTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using ZeroMQ;
+
+namespace ZeroMQTest
+{
+    [TestFixture]
+    public class ZMessageTest
+    {
+        [Test]
+        public void Create_Default()
+        {
+            using (var msg = new ZMessage())
+            {
+                Assert.AreEqual(0, msg.Count);
+            }
+        }
+
+        [Test]
+        public void Create_FromFrames_Empty()
+        {
+            using (var msg = new ZMessage(new ZFrame[] { }))
+            {
+                Assert.AreEqual(0, msg.Count);
+            }
+        }
+
+        [Test]
+        public void Create_FromFrames_NonEmpty()
+        {
+            using (var msg = new ZMessage(new ZFrame[] { new ZFrame() }))
+            {
+                Assert.AreEqual(1, msg.Count);
+            }
+        }
+    }
+}
