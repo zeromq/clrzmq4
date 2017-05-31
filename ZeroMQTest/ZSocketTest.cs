425650bae6e34d1fa59173549e64e0a75bf0df7c
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-05-31 18:08:02 +0200

    Added some tests for ZSocket

---
+++ ZeroMQTest/ZSocketTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ZeroMQ;
+
+namespace ZeroMQTest
+{
+    [TestFixture]
+    public class ZSocketTest
+    {
+        static IEnumerable<ZSocketType> ValidSocketTypes { get { return Enum.GetValues(typeof(ZSocketType)).Cast<ZSocketType>().Except(new[] { ZSocketType.None }); } }
+
+        [Test, TestCaseSource(nameof(ValidSocketTypes))]
+        public void Create_WithContext(ZSocketType socketType)
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, socketType))
+                {
+                }
+            }
+        }
+
+        [Test]
+        public void Create_WithContext_None_Fails()
+        {
+            using (var context = new ZContext())
+            {
+                // TODO: Should this be converted to ArgumentException?
+                Assert.Throws<ZException>(() => new ZSocket(context, ZSocketType.None));
+            }
+        }
+    }
+}
