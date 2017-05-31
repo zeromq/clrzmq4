42c8d8a29fa91bf304ac1c74d8f0c1f8575bbb7f
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-05-31 17:20:20 +0200

    Added some tests for ZContext

---
+++ ZeroMQTest/ZContextTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using ZeroMQ;
+
+namespace ZeroMQTest
+{
+    [TestFixture]
+    public class ZContextTest
+    {
+        [Test]
+        public void Create_ViaFactoryMethod()
+        {
+            using (var context = ZContext.Create())
+            {
+                Assert.IsNotNull(context);
+            }
+        }
+
+        [Test]
+        public void Create_ViaCtor()
+        {
+            using (var context = new ZContext())
+            {
+                Assert.IsNotNull(context);
+            }
+        }
+
+        [Test]
+        public void StaticInstance()
+        {
+            // TODO: Is it a good idea to supply a static instance in the library?
+            Assert.IsNotNull(ZContext.Current);
+        }
+
+        [Test]
+        public void Terminate()
+        {
+            using (var context = new ZContext())
+            {
+                context.Terminate();
+            }
+        }
+
+        [Test]
+        public void Terminate_Twice()
+        {
+            using (var context = new ZContext())
+            {
+                context.Terminate();
+                context.Terminate();
+            }
+        }
+
+        [Test]
+        public void Shutdown()
+        {
+            using (var context = new ZContext())
+            {
+                context.Shutdown();
+            }
+        }
+
+        [Test]
+        public void Shutdown_Twice()
+        {
+            using (var context = new ZContext())
+            {
+                context.Shutdown();
+                context.Shutdown();
+            }
+        }
+    }
+}
