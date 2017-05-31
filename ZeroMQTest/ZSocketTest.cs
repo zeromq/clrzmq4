8effd9b5c7d095cc0663a7c1b8d0ae230bc556fc
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-05-31 19:14:13 +0200

    Added more tests for ZSocket

--- ZeroMQTest/ZSocketTest.cs
+++ ZeroMQTest/ZSocketTest.cs
@@ -18,6 +18,7 @@ namespace ZeroMQTest
             {
                 using (var socket = new ZSocket(context, socketType))
                 {
+                    Assert.AreSame(context, socket.Context);
                 }
             }
         }
@@ -31,5 +32,178 @@ namespace ZeroMQTest
                 Assert.Throws<ZException>(() => new ZSocket(context, ZSocketType.None));
             }
         }
+
+        [Test, TestCaseSource(nameof(ValidSocketTypes))]
+        public void Close(ZSocketType socketType)
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, socketType))
+                {
+                    socket.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// TODO: this behaviour is deviating from that of libzmq and it may mask erroneous uses. an exception should probably be thrown
+        /// </summary>
+        /// <param name="socketType"></param>
+        [Test, TestCaseSource(nameof(ValidSocketTypes))]
+        public void Close_Twice(ZSocketType socketType)
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, socketType))
+                {
+                    socket.Close();
+                    socket.Close();
+                }
+            }
+        }
+
+        #region socket options
+        [Test]
+        public void GetOption_Affinity()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    Assert.AreEqual(0, socket.Affinity);
+                }
+            }
+        }
+
+        [Test]
+        public void GetOption_Backlog()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    Assert.AreEqual(100, socket.Backlog);
+                }
+            }
+        }
+
+        [Test]
+        public void GetOption_Conflate()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    Assert.AreEqual(false, socket.Conflate);
+                }
+            }
+        }
+
+        [Test]
+        public void GetOption_ConnectRID()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.ROUTER))
+                {
+                    // TODO: Probably this socket option cannot be queried. The property getter should be removed.
+                    Assert.Throws<ZException>(() => { var x = socket.ConnectRID; });
+                }
+            }
+        }
+
+        [Test, Ignore("Issue in underlying libzmq fixed in commit https://github.com/zeromq/libzmq/commit/f86795350d2c37753b961018b5185cd1af33a38a")]
+        public void GetOption_CurvePublicKey()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    CollectionAssert.AreEqual(Enumerable.Repeat<byte>(0, ZSocket.BinaryKeySize).ToArray(), socket.CurvePublicKey);
+                }
+            }
+        }
+
+        [Test, Ignore("Issue in underlying libzmq fixed in commit https://github.com/zeromq/libzmq/commit/f86795350d2c37753b961018b5185cd1af33a38a")]
+        public void GetOption_CurveSecretKey()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    CollectionAssert.AreEqual(Enumerable.Repeat<byte>(0, ZSocket.BinaryKeySize).ToArray(), socket.CurveSecretKey);
+                }
+            }
+        }
+
+        [Test, Ignore("Issue in underlying libzmq fixed in commit https://github.com/zeromq/libzmq/commit/f86795350d2c37753b961018b5185cd1af33a38a")]
+        public void GetOption_CurveServerKey()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    CollectionAssert.AreEqual(Enumerable.Repeat<byte>(0, ZSocket.BinaryKeySize).ToArray(), socket.CurveServerKey);
+                }
+            }
+        }
+
+        [Test]
+        public void SetOption_CurvePublicKey()
+        {
+            if (!ZContext.Has("curve")) Assert.Ignore("Ignored due to missing curve support");
+
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    // TODO: the interface is confusing; Z85-encoded keys should always be strings
+                    byte[] publicKeyZ85, secretKeyZ85;
+                    Z85.CurveKeypair(out publicKeyZ85, out secretKeyZ85);
+                    socket.CurvePublicKey = publicKeyZ85;
+                    byte[] publicKeyBinary = Z85.Decode(publicKeyZ85);
+                    CollectionAssert.AreEqual(publicKeyBinary, socket.CurvePublicKey);
+                }
+            }
+        }
+
+        [Test]
+        public void SetOption_CurveServerKey()
+        {
+            if (!ZContext.Has("curve")) Assert.Ignore("Ignored due to missing curve support");
+
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    // TODO: the interface is confusing; Z85-encoded keys should always be strings
+                    byte[] publicKeyZ85, secretKeyZ85;
+                    Z85.CurveKeypair(out publicKeyZ85, out secretKeyZ85);
+                    socket.CurveServerKey = publicKeyZ85;
+                    byte[] publicKeyBinary = Z85.Decode(publicKeyZ85);
+                    CollectionAssert.AreEqual(publicKeyBinary, socket.CurveServerKey);
+                }
+            }
+        }
+
+        [Test]
+        public void SetOption_CurveSecretKey()
+        {
+            if (!ZContext.Has("curve")) Assert.Ignore("Ignored due to missing curve support");
+
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    // TODO: the interface is confusing; Z85-encoded keys should always be strings
+                    byte[] publicKeyZ85, secretKeyZ85;
+                    Z85.CurveKeypair(out publicKeyZ85, out secretKeyZ85);
+                    socket.CurveSecretKey = secretKeyZ85;
+                    byte[] secretKeyBinary = Z85.Decode(secretKeyZ85);
+                    CollectionAssert.AreEqual(secretKeyBinary, socket.CurveSecretKey);
+                }
+            }
+        }
+        #endregion
     }
 }
