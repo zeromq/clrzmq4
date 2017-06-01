57148c2dc8d4b820b56333d6db64695492ccaea6
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-06-01 10:46:32 +0200

    Added more tests for ZPoll*

--- ZeroMQTest/ZPollTest.cs
+++ ZeroMQTest/ZPollTest.cs
@@ -8,6 +8,40 @@ namespace ZeroMQTest
     [TestFixture]
     public class ZPollTest
     {
+        [Test]
+        public void PollSingle_Timeout()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    ZMessage message = null;
+                    ZError error;
+                    Assert.IsFalse(socket.Poll(ZPollItem.CreateReceiver(), ZPoll.In, ref message, out error, TimeSpan.Zero));
+
+                    Assert.IsNull(message);
+                    Assert.AreEqual(ZError.EAGAIN, error);
+                }
+            }
+        }
+
+        [Test]
+        public void PollInSingle_Timeout()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    ZMessage message;
+                    ZError error;
+                    Assert.IsFalse(socket.PollIn(ZPollItem.CreateReceiver(), out message, out error, TimeSpan.Zero));
+
+                    Assert.IsNull(message);
+                    Assert.AreEqual(ZError.EAGAIN, error);
+                }
+            }
+        }
+
         [Test, Ignore("TODO: this behaviour must be revised")]
         public void PollMany_Empty()
         {
@@ -17,6 +51,26 @@ namespace ZeroMQTest
             ZMessage[] messages = null;
             ZError error;
             Assert.IsFalse(sockets.Poll(pollItems, ZPoll.In, ref messages, out error));
+        }
+
+        [Test]
+        public void PollInMany_Single_Timeout()
+        {
+            using (var context = new ZContext())
+            {
+                using (var socket = new ZSocket(context, ZSocketType.PAIR))
+                {
+                    var sockets = new[] { socket };
+                    var pollItems = new[] { ZPollItem.CreateReceiver() };
+
+                    ZMessage[] messages;
+                    ZError error;
+                    Assert.IsFalse(sockets.PollIn(pollItems, out messages, out error, TimeSpan.Zero));
+
+                    CollectionAssert.AreEqual(new ZMessage[] { null }, messages);
+                    Assert.AreEqual(ZError.EAGAIN, error);
+                }
+            }
         }
 
         [Test]
@@ -32,6 +86,9 @@ namespace ZeroMQTest
                     ZMessage[] messages = null;
                     ZError error;
                     Assert.IsFalse(sockets.Poll(pollItems, ZPoll.In, ref messages, out error, TimeSpan.Zero));
+
+                    CollectionAssert.AreEqual(new ZMessage[] { null }, messages);
+                    Assert.AreEqual(ZError.EAGAIN, error);
                 }
             }
         }
@@ -55,6 +112,9 @@ namespace ZeroMQTest
                         ZMessage[] messages = null;
                         ZError error;
                         Assert.IsTrue(sockets.Poll(pollItems, ZPoll.In, ref messages, out error, TimeSpan.Zero));
+
+                        CollectionAssert.AreEqual(new[] { new ZMessage(new[] { new ZFrame(32) }) }, messages);
+                        Assert.IsNull(error);
                     }
                 }
             }
