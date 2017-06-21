620f98c41f39cf773ee4ccd3ab5a8a47c0fdddf2
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-06-21 13:13:34 +0200

    Initial attempt to add mono ci using travis-ci

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then
+  rm -R $artifactsFolder
+fi
+
+nuget restore
+
+nuget install NUnit.Console -Version 3.6.1 -OutputDirectory testrunner
+
+xbuild /p:Configuration=Release clrzmq4.mono.sln
+mono ./testrunner/NUnit.Console.3.6.1/tools/nunit-console.exe ./bin/Release/ZeroMQ.Test.dll
+
+# TODO add MonoCov
+
+revision=${TRAVIS_JOB_ID:=1}
+revision=$(printf "%04d" $revision)
+
+#nuget pack ./src/Invio.Extensions.DependencyInjection -c Release -o ./artifacts
