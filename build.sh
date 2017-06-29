bc0cb39232a586fa7a8679c819ed5f5da0b01657
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-06-29 10:59:59 +0200

    Use specific (non-broken) macports archives mirrors only

--- build.sh
+++ build.sh
@@ -16,11 +16,13 @@ if [ "$(sw_vers -productName)" == "Mac OS X" ] ; then
   wget --retry-connrefused --waitretry=1 -O /tmp/macports.pkg https://github.com/macports/macports-base/releases/download/v2.4.1/MacPorts-2.4.1-10.11-ElCapitan.pkg
   sudo installer -pkg /tmp/macports.pkg -target /
   export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+  sudo rm /opt/local/etc/macports/archive_sites.conf
   sudo echo "name macports_archives" >/opt/local/etc/macports/archive_sites.conf
   sudo echo "name local_archives" >>/opt/local/etc/macports/archive_sites.conf
   sudo echo "urls http://packages.macports.org/ http://nue.de.packages.macports.org/" >>/opt/local/etc/macports/archive_sites.conf
   sudo port install zmq +universal
-  file /usr/local/lib/*.dylib
+  file /usr/local/lib/*mq*.dylib
+  file /opt/local/lib/*mq*.dylib
   find /usr/local -name '*zmq*' # DEBUG
   find /usr/local -name '*zeromq*' # DEBUG
   echo DYLD_LIBRARY_PATH=$DYDYLD_LIBRARY_PATH
