3b87b5d39a58abd139e028fbdbca13391b0dcafa
metadings <metadings@live.de>
2017-09-17 13:14:59 +0200

    Updating Platform.MacOSX, Platform.Posix: Don't use .a files

--- lib/Platform.MacOSX.cs
+++ lib/Platform.MacOSX.cs
@@ -8,32 +8,15 @@
 
 			// public const string LibraryFileExtension = ".dylib";
 
-            // TODO: is the redundancy here really necessary? Why look for .a files, which are static libraries? Why look for .so files? are they used at all on Mac OS X?
 			public static readonly string[] LibraryPaths = new string[] {
 				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.dylib",
 				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.dylib.*",
-				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.a",
-				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.a.*",
-				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.so",
-				"{AppBase}/{Arch}/{Compiler}/{LibraryName}.so.*",
 				"{AppBase}/{Arch}/{LibraryName}.dylib",
 				"{AppBase}/{Arch}/{LibraryName}.dylib.*",
-				"{AppBase}/{Arch}/{LibraryName}.a",
-				"{AppBase}/{Arch}/{LibraryName}.a.*",
-				"{AppBase}/{Arch}/{LibraryName}.so",
-				"{AppBase}/{Arch}/{LibraryName}.so.*",
 				"{Path}/{LibraryName}.dylib",
 				"{Path}/{LibraryName}.dylib.*",
-				"{Path}/{LibraryName}.a",
-				"{Path}/{LibraryName}.a.*",
-				"{Path}/{LibraryName}.so",
-				"{Path}/{LibraryName}.so.*",
                 "{DllPath}/{LibraryName}.dylib",
                 "{DllPath}/{LibraryName}.dylib.*",
-                "{LibraryName}.dylib",
-                "@rpath/{LibraryName}.dylib",
-                "/usr/local/lib/{LibraryName}.dylib", // DEBUG: remove this again
-                "/opt/local/lib/{LibraryName}.dylib", // DEBUG: remove this again
             };
 
 		}
