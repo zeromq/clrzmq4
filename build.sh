1385f4a069cc5d3c865b775cca20e91ccded270d
Simon Giesecke <simon.giesecke@btc-ag.com>
2017-09-19 09:33:11 +0200

    Problem: repository binaries are always overwritten by macports
    Solution: make macports installation optional; separated install_deps.sh
    from build.sh

--- build.sh
+++ build.sh
@@ -9,41 +9,6 @@ if [ -d $artifactsFolder ]; then
   rm -R $artifactsFolder
 fi
 
-if [ "$(uname)" == "Darwin" ] ; then
-  # DIAGNOSTICS
-  otool -L amd64/libzmq.dylib
-  otool -L i386/libzmq.dylib
-  
-  # homebrew has zeromq only as x64 as of 2017-06-29, so we must use macports (see also https://github.com/travis-ci/travis-ci/issues/5640)
-  #brew install zeromq --universal
-  wget --retry-connrefused --waitretry=1 -O /tmp/macports.pkg https://github.com/macports/macports-base/releases/download/v2.4.1/MacPorts-2.4.1-10.11-ElCapitan.pkg
-  sudo installer -pkg /tmp/macports.pkg -target /
-  export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-  sudo rm /opt/local/etc/macports/archive_sites.conf
-  echo "name macports_archives" >archive_sites.conf
-  echo "name local_archives" >>archive_sites.conf
-  echo "urls http://packages.macports.org/ http://nue.de.packages.macports.org/" >>archive_sites.conf
-  sudo cp archive_sites.conf /opt/local/etc/macports/
-  
-  # ignore errors on call to port, since this seems to always fail with "Updating database of binaries failed"  
-  while (sudo port -v install zmq +universal || true) | grep "Error: Port zmq not found" ; do echo "port install zmq failed, retrying" ; done
-  
-  file /opt/local/lib/*mq*.dylib # DIAGNOSTICS
-  
-  cp /opt/local/lib/libzmq.dylib amd64
-  cp /opt/local/lib/libzmq.dylib i386  
-else
-  # assume that we are on Ubuntu (which is used on Travis-CI.org)
-  curl http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/xUbuntu_14.04/Release.key >Release.key
-  sudo apt-key add Release.key
-  sudo add-apt-repository "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/xUbuntu_14.04 ./"
-  sudo apt-get update
-  sudo apt-get install libzmq3-dev
-fi
-
-nuget install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory testrunner
-# nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
-
 xbuild /p:Configuration=Release clrzmq4.mono.sln
 
 export MONO_TRACE_LISTENER=Console.Out
