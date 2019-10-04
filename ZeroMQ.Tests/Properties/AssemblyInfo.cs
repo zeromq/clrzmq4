57f6cc90297d121750fd1cdc5c55b9acdad3cab3
Jonas Lidgren <sm2vsd@lidgren.se>
2019-10-04 15:22:13 +0200

    Added Tests project and some tests for ZCert

---
+++ ZeroMQ.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZeroMQ.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZeroMQ.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b57e66f4-b77e-48a1-a7d0-eb551e357111")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ZeroMQ.Tests/ZCertTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZeroMQ;
+
+
+namespace ZeroMQ.Tests
+{
+    [TestClass]
+    public class ZCertTest
+    {
+        [TestMethod]
+        public void TestConstructors()
+        {
+            ZCert cert1 = new ZCert();
+
+            String pub_key = cert1.PublicTxt;
+            String sec_key = cert1.SecretTxt;
+
+            Console.WriteLine("Generated public key: " + pub_key);
+            Console.WriteLine("Generated secret key: " + sec_key);
+
+            /*
+            * Test string constructor
+            */
+            ZCert cert2 = new ZCert(pub_key, sec_key);
+            
+            CollectionAssert.AreEqual(cert1.PublicKey, cert2.PublicKey);
+            CollectionAssert.AreEqual(cert1.SecretKey, cert2.SecretKey);
+            Assert.IsTrue(cert1.Equals(cert2));
+
+            /*
+            * Test byte constructor
+            */
+            
+            ZCert cert3 = new ZCert(cert1.PublicKey, cert1.SecretKey);
+
+            Assert.AreEqual(cert1.PublicTxt, cert3.PublicTxt);
+            Assert.AreEqual(cert1.SecretTxt, cert3.SecretTxt);
+
+            CollectionAssert.AreEqual(cert1.PublicKey, cert3.PublicKey);
+            CollectionAssert.AreEqual(cert1.SecretKey, cert3.SecretKey);
+            Assert.IsTrue(cert1.Equals(cert3));
+        }
+
+        [TestMethod]
+        public void TestDuplicate()
+        {
+            ZCert cert1 = new ZCert();
+            ZCert cert2 = ZCert.Dup(cert1);
+            Assert.IsTrue(cert1.Equals(cert2));
+        }
+    }
+}
